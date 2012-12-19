435e9ef943c3fc702377447198220ddfd8131cac
Benjamin Jackson <bhjackson@gmail.com>
2012-12-19 16:50:10 -0500

    Added twitter config file for oauth config and lib_trollop for options parsing.

---
+++ config/twitter.rb
@@ -0,0 +1,6 @@
+Twitter.configure do |config|
+  config.consumer_key = "YOUR_CONSUMER_KEY"
+  config.consumer_secret = "YOUR_CONSUMER_SECRET"
+  config.oauth_token = "YOUR_OAUTH_TOKEN"
+  config.oauth_token_secret = "YOUR_OAUTH_TOKEN_SECRET"
+end
