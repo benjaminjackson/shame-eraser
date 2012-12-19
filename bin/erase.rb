0d77b692479209703ff9d8457ca1fd62e37b91c8
Benjamin Jackson <bhjackson@gmail.com>
2012-12-19 16:50:35 -0500

    Added trollop options to bin file.

--- bin/erase.rb
+++ bin/erase.rb
@@ -2,3 +2,10 @@ require 'rubygems'
 require "bundler/setup"
 
 require 'twitter'
+require File.join(File.dirname(__FILE__), "lib", "lib_trollop")
+
+opts = Trollop::options do
+  opt :"start-date", "Tweets before this date will not be deleted", :short => 's', :type => Date, :required => true
+  opt :"end-date", "Tweets after this date will not be deleted", :short => 'e', :type => Date :required => true
+end
+
